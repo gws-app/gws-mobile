1a1bca825151299a85b4bcc4944e895caa093fd6
FARABI <ilhamfarabi30@gmail.com>
2024-12-09 09:02:50 +0700

    feat: update theme and adjust navigation icons

--- app/src/main/java/com/gws/gws_mobile/MainActivity.kt
+++ app/src/main/java/com/gws/gws_mobile/MainActivity.kt
@@ -4,16 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.gws.gws_mobile.databinding.ActivityMainBinding
 import com.gws.gws_mobile.ui.chatbot.ChatbotActivity
+import com.gws.gws_mobile.ui.settings.SettingPreferences
+import com.gws.gws_mobile.ui.settings.SettingsViewModel
+import com.gws.gws_mobile.ui.settings.ViewModelFactory
+import com.gws.gws_mobile.ui.settings.dataStore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var settingsViewModel: SettingsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +29,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Get Theme Status
+        val pref = SettingPreferences.getInstance(dataStore)
+        val factory = ViewModelFactory(pref)
+        settingsViewModel = ViewModelProvider(this, factory)[SettingsViewModel::class.java]
+
+        settingsViewModel.getThemeSettings().observe(this) { isDarkModeActive ->
+            AppCompatDelegate.setDefaultNightMode(
+                if (isDarkModeActive) AppCompatDelegate.MODE_NIGHT_YES
+                else AppCompatDelegate.MODE_NIGHT_NO
+            )
+        }
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
@@ -29,7 +48,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_insight, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_insight, R.id.navigation_settings
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/gws/gws_mobile/ui/settings/SettingsFragment.kt
@@ -0,0 +1,46 @@
+package com.gws.gws_mobile.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.gws.gws_mobile.R
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var settingsViewModel: SettingsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val pref = SettingPreferences.getInstance(requireContext().dataStore)
+        val factory = ViewModelFactory(pref)
+
+        settingsViewModel = ViewModelProvider(this, factory)[SettingsViewModel::class.java]
+        val switchTheme = view.findViewById<SwitchMaterial>(R.id.switch_theme)
+        settingsViewModel.getThemeSettings().observe(viewLifecycleOwner) { isDarkModeActive ->
+            switchTheme.isChecked = isDarkModeActive
+        }
+
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            settingsViewModel.saveThemeSetting(isChecked)
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+}
