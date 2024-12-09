9b83c5f843c137cf4cbeff38e5978b89102d2ed6
FARABI <ilhamfarabi30@gmail.com>
2024-12-09 22:00:35 +0700

    fix: resolve jedag jedug issue in dark mode

--- app/src/main/java/com/gws/gws_mobile/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/gws/gws_mobile/ui/settings/SettingsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.switchmaterial.SwitchMaterial
@@ -25,22 +24,24 @@ class SettingsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val switchTheme = view.findViewById<SwitchMaterial>(R.id.switch_theme)
+
         val pref = SettingPreferences.getInstance(requireContext().dataStore)
         val factory = ViewModelFactory(pref)
-
         settingsViewModel = ViewModelProvider(this, factory)[SettingsViewModel::class.java]
-        val switchTheme = view.findViewById<SwitchMaterial>(R.id.switch_theme)
+
         settingsViewModel.getThemeSettings().observe(viewLifecycleOwner) { isDarkModeActive ->
             switchTheme.isChecked = isDarkModeActive
+            val nightMode = if (isDarkModeActive) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+            AppCompatDelegate.setDefaultNightMode(nightMode)
         }
 
-        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+        switchTheme.setOnCheckedChangeListener { _, isChecked ->
             settingsViewModel.saveThemeSetting(isChecked)
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            }
         }
     }
 }
