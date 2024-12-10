2bb4380cb8841d0daafa20121f9b4d8f375a4802
hildanku <hildankutomo@gmail.com>
2024-12-10 20:03:22 +0700

    chore(AddMoodActivity): implement toggleCollapse for other button

--- app/src/main/java/com/gws/gws_mobile/ui/home/addmood/AddMoodActivity.kt
+++ app/src/main/java/com/gws/gws_mobile/ui/home/addmood/AddMoodActivity.kt
@@ -28,6 +28,27 @@ class AddMoodActivity : AppCompatActivity() {
         binding.btnSocialInteraction.setOnClickListener {
             toggleCollapseExpand(binding.llSocialInteractionContent, binding.btnSocialInteraction, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
         }
+
+        binding.btnCollapseWorkStudy.setOnClickListener {
+            toggleCollapseExpand(binding.llWorkStudyContent, binding.btnCollapseWorkStudy, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
+        }
+
+        binding.btnCollapseFoodDrink.setOnClickListener {
+            toggleCollapseExpand(binding.llFoodDrinkContent, binding.btnCollapseFoodDrink, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
+        }
+
+        binding.btnCollapseNatureOutdoor.setOnClickListener {
+            toggleCollapseExpand(binding.llNatureOutdoorContent, binding.btnCollapseNatureOutdoor, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
+        }
+
+        binding.btnCollapseEntertainment.setOnClickListener {
+            toggleCollapseExpand(binding.llEntertainmentContent, binding.btnCollapseEntertainment, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
+        }
+
+        binding.btnCollapseMindfulness.setOnClickListener {
+            toggleCollapseExpand(binding.llMindfulnessContent, binding.btnCollapseMindfulness, R.drawable.ic_arrow_drop_up_24, R.drawable.ic_arrow_drop_down_24)
+        }
+
     }
 
     private fun toggleCollapseExpand(contentView: View, button: View, collapseIcon: Int, expandIcon: Int) {
