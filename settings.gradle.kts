9ee013a1d39e92e869106d73ca281414f4348ac1
FARABI <ilhamfarabi30@gmail.com>
2024-12-13 10:42:32 +0700

    chore: update logo and dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
                 includeGroupByRegex("androidx.*")
             }
         }
-        jcenter()
+        mavenCentral()
         gradlePluginPortal()
     }
 }
@@ -15,7 +15,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        jcenter()
+        mavenCentral()
         maven {
             url = uri("https://jitpack.io")
         }
